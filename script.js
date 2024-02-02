f8a7150682d14dff488699423c34096dd0e92ba5
shany-shan <priyaeswaran2023@gmail.com>
2024-02-02 11:34:02 +0530

    displaying the  currentlocation on the map with marker usinng leaflet library

--- script.js
+++ script.js
@@ -22,3 +22,30 @@ const inputDistance = document.querySelector(".form__input--distance");
 const inputDuration = document.querySelector(".form__input--duration");
 const inputCadence = document.querySelector(".form__input--cadence");
 const inputElevation = document.querySelector(".form__input--elevation");
+
+//getting the current location coords using geolocation API
+if (navigator.geolocation) {
+  navigator.geolocation.getCurrentPosition(
+    function (position) {
+      const { latitude, longitude } = position.coords;
+      console.log(latitude, longitude);
+      //using these coords with googlemap
+      //const gmap = `https://www.google.com/maps/@${latitude},${longitude},12z?entry=ttu`;
+
+      const coords = [latitude, longitude];
+      //leaflet starter code
+      //creating a map obj using leaflet lib
+      var map = L.map("map").setView(coords, 13);
+
+      L.tileLayer("https://tile.openstreetmap.fr/hot/{z}/{x}/{y}.png", {
+        attribution:
+          '&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors',
+      }).addTo(map);
+
+      L.marker(coords).addTo(map).bindPopup("yet to define ").openPopup();
+    },
+    function () {
+      alert("Allow to access the Location");
+    }
+  );
+}
